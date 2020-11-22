841434c1256dc0fce241b6decbbbea583f0b77a7
Maxym Berezanskyi <m.berezanskyi@bmlabs.org>
2020-11-22 11:06:54 +0200

    Added authorization

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
 const Header = () => {
+  const { logIn, logOut, isAuthenticated, currentUser } = useAuth()
+
   return (
     <header>
       <h1>
@@ -13,6 +16,12 @@ const Header = () => {
           </li>
           <li>
             <Link to={routes.contact()}>Contact Us</Link>
+          </li>
+          {isAuthenticated && <li>{currentUser.email}</li>}
+          <li>
+            <button onClick={isAuthenticated ? logOut : logIn}>
+              {isAuthenticated ? 'Log Out' : 'Log In'}
+            </button>
           </li>
         </ul>
       </nav>
